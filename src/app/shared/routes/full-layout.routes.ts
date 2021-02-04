3598e0a2d413bde6a5e008f8f4d2aac2452ebebc
achavezc <achavezc146@gmail.com>
2021-02-03 22:00:01 -0500

    ajustes

--- src/app/shared/routes/full-layout.routes.ts
+++ src/app/shared/routes/full-layout.routes.ts
@@ -6,5 +6,9 @@ export const Full_ROUTES: Routes = [
   {
     path: 'dashboard',
     loadChildren: () => import('../../pages/dashboard/dashboard.module').then(m => m.DashboardModule)
+  },
+  {
+    path: 'acopio',
+    loadChildren: () => import('../../acopio/acopio.module').then(m => m.AcopioModule)
   }
 ];
