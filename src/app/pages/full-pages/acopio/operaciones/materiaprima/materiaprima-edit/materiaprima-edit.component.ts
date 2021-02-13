c05bb6667d653a475cbd928eb37653615fcccc48
Pamela <chuancahuari@outlook.com>
2021-02-13 15:26:29 -0500

    proveedor

--- src/app/pages/full-pages/acopio/operaciones/materiaprima/materiaprima-edit/materiaprima-edit.component.ts
+++ src/app/pages/full-pages/acopio/operaciones/materiaprima/materiaprima-edit/materiaprima-edit.component.ts
@@ -2,6 +2,9 @@ import { Component, OnInit, ViewEncapsulation,Input,ViewChild } from '@angular/c
 import { NgbModal, ModalDismissReasons, NgbActiveModal } from '@ng-bootstrap/ng-bootstrap';
 import { DatatableComponent, ColumnMode } from "@swimlane/ngx-datatable";
 import { materiaPrimaListData } from "../../materiaprima/materiaprima-list/data/materiaprima-list.data";
+import { MaestroService } from '../../../../../../services/maestro.service';
+import { Observable } from 'rxjs';
+import { FormControl, FormGroup, Validators, ValidationErrors, ValidatorFn } from '@angular/forms';
 
 @Component({
   selector: 'app-materiaprima-list',
@@ -10,31 +13,44 @@ import { materiaPrimaListData } from "../../materiaprima/materiaprima-list/data/
   encapsulation: ViewEncapsulation.None
 })
 export class MateriaPrimaEditComponent implements OnInit {
+  
+  @ViewChild('vform') validationForm: FormGroup;
   @Input() name;
   closeResult: string;
-
+  consultaMateriaPrimaForm: FormGroup;
+  listaProducto: Observable<any[]>;
+  listaSubProducto: Observable<any[]>;
   selectedEstado: any;
+  selectProducto: any;
+  selectSubProducto: any;
   popupModel;
-  listaEstado = [
-    { id: 1, name: 'Anulado' },
-    { id: 2, name: 'Pesado' },
-    { id: 3, name: 'Analizado' }
-];
+  
 @ViewChild(DatatableComponent) table: DatatableComponent;
 
-    public rows = materiaPrimaListData;
+    
     public ColumnMode = ColumnMode;
     public limitRef = 10;
-    private tempData = [];
 
 
-constructor(private modalService: NgbModal) { 
-  this.tempData = materiaPrimaListData;
+constructor(private modalService: NgbModal, private maestroService: MaestroService) { 
+ 
 }
 
   ngOnInit(): void {
+    this.cargarForm();
+    this.cargarcombos();
   }
 
+  cargarForm() {
+    this.consultaMateriaPrimaForm = new FormGroup(
+      {
+        numGuia: new FormControl('', []),
+        numReferencia: new FormControl('', []),
+        producto: new FormControl('', []),
+        subproducto: new FormControl('', [])
+      });
+    //this.consultaMateriaPrimaForm.setValidators(this.comparisonValidator())
+  }
   open(content) {
     this.modalService.open(content).result.then((result) => {
         this.closeResult = `Closed with: ${result}`;
@@ -52,5 +68,31 @@ private getDismissReason(reason: any): string {
         return `with: ${reason}`;
     }
 }
+
+cargarcombos() {
+  this.maestroService.obtenerMaestros("Producto", 1)
+    .subscribe(res => {
+      if (res.Result.Success) {
+        this.listaProducto = res.Result.Data;
+      }
+    },
+      err => {
+        console.error(err);
+      }
+    );
+
+  this.maestroService.obtenerMaestros("SubProducto", 1)
+    .subscribe(res => {
+      if (res.Result.Success) {
+        this.listaSubProducto = res.Result.Data;
+      }
+    },
+      err => {
+        console.error(err);
+      }
+    );
+
+}
+
 
 }
