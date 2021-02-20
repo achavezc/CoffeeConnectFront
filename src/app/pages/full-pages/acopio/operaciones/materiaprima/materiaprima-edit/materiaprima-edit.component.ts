41c42fd80cbbc9f2cf9bb2ec0eb33c0a3ca1f0a5
achavezc <achavezc146@gmail.com>
2021-02-20 17:06:02 -0500

    ajustes

--- src/app/pages/full-pages/acopio/operaciones/materiaprima/materiaprima-edit/materiaprima-edit.component.ts
+++ src/app/pages/full-pages/acopio/operaciones/materiaprima/materiaprima-edit/materiaprima-edit.component.ts
@@ -10,6 +10,7 @@ import { host } from '../../../../../../shared/hosts/main.host';
 import { ILogin } from '../../../../../../services/models/login';
 import { MaestroUtil } from '../../../../../../services/util/maestro-util';
 import { NgSelectComponent } from '@ng-select/ng-select';
+import { AlertUtil } from '../../../../../../services/util/alert-util';
 
 @Component({
   selector: 'app-materiaprima-list',
@@ -22,6 +23,7 @@ export class MateriaPrimaEditComponent implements OnInit {
   @ViewChild('vform') validationForm: FormGroup;
   @Input() name;
   submitted = false;
+  submittedEdit = false;
   closeResult: string;
   consultaMateriaPrimaFormEdit: FormGroup;
   consultaProveedor: FormGroup;
@@ -50,6 +52,7 @@ export class MateriaPrimaEditComponent implements OnInit {
   @ViewChild(DatatableComponent) tableProveedor: DatatableComponent;
 
   constructor(private modalService: NgbModal, private maestroService: MaestroService, private filtrosProveedor: FiltrosProveedor,
+    private alertUtil: AlertUtil,
     private spinner: NgxSpinnerService, private acopioService: AcopioService, private maestroUtil: MaestroUtil,) {
     this.singleSelectCheck = this.singleSelectCheck.bind(this);
   }
@@ -67,8 +70,8 @@ export class MateriaPrimaEditComponent implements OnInit {
       {
         numGuia: new FormControl('', []),
         numReferencia: new FormControl('', []),
-        producto: new FormControl('', []),
-        subproducto: new FormControl('', []),
+        producto: new FormControl('', [Validators.required]),
+        subproducto: new FormControl('', [Validators.required]),
         provNombre: new FormControl('', []),
         provDocumento: new FormControl('', []),
         provTipoSocio: new FormControl({disabled: true}, []),
@@ -76,7 +79,8 @@ export class MateriaPrimaEditComponent implements OnInit {
         provDepartamento: new FormControl('', []),
         provProvincia: new FormControl('', []),
         provDistrito: new FormControl('', []),
-        provZona: new FormControl('', [])
+        provZona: new FormControl('', []),
+        fechaPesado: new FormControl('', [Validators.required])
       });
   }
   /*open(content) {
@@ -189,6 +193,9 @@ export class MateriaPrimaEditComponent implements OnInit {
   get f() {
     return this.consultaProveedor.controls;
   }
+  get fedit() {
+    return this.consultaMateriaPrimaFormEdit.controls;
+  }
   public comparisonValidator(): ValidatorFn {
     return (group: FormGroup): ValidationErrors => {
       const tipoproveedor = group.controls['tipoproveedor'];
@@ -293,7 +300,59 @@ export class MateriaPrimaEditComponent implements OnInit {
     link.remove();
   }
   guardar(){
-    alert("guardado");
+    if (this.consultaMateriaPrimaFormEdit.invalid) {
+      this.submittedEdit = true;
+      return;
+    } else {
+
+      this.submittedEdit = false;
+      /* this.consultaMateriaPrimaFormEdit.Numero = this.consultaMateriaPrimaFormEdit.controls['numeroGuia'].value;
+      this.consultaMateriaPrimaFormEdit.NombreRazonSocial = this.consultaMateriaPrimaFormEdit.controls['nombre'].value;
+      this.consultaMateriaPrimaFormEdit.TipoDocumentoId = this.consultaMateriaPrimaFormEdit.controls['tipoDocumento'].value;
+      this.consultaMateriaPrimaFormEdit.NumeroDocumento = this.consultaMateriaPrimaFormEdit.controls['numeroDocumento'].value;
+      this.consultaMateriaPrimaFormEdit.ProductoId = this.consultaMateriaPrimaFormEdit.controls['producto'].value;
+      this.consultaMateriaPrimaFormEdit.CodigoSocio = this.consultaMateriaPrimaFormEdit.controls['codigoSocio'].value;
+      this.consultaMateriaPrimaFormEdit.EstadoId = this.consultaMateriaPrimaFormEdit.controls['estado'].value;
+      this.consultaMateriaPrimaFormEdit.FechaInicio = this.consultaMateriaPrimaFormEdit.controls['fechaInicio'].value;
+      this.consultaMateriaPrimaFormEdit.FechaFin = this.consultaMateriaPrimaFormEdit.controls['fechaFin'].value;
+      this.spinner.show(undefined,
+        {
+          type: 'ball-triangle-path',
+          size: 'medium',
+          bdColor: 'rgba(0, 0, 0, 0.8)',
+          color: '#fff',
+          fullScreen: true
+        });
+      this.acopioService.consultarMateriaPrima(this.consultaMateriaPrimaFormEdit)
+        .subscribe(res => {
+          this.spinner.hide();
+          if (res.Result.Success) {
+            if (res.Result.ErrCode == "") {
+              res.Result.Data.forEach(obj => {
+
+                var fecha = new Date(obj.FechaRegistro);
+                obj.FechaRegistroCadena = fecha.getUTCDate() + "/" + fecha.getUTCMonth() + 1 + "/" + fecha.getUTCFullYear();
+
+              });
+              this.tempData = res.Result.Data;
+              this.rows = [...this.tempData];
+              this.selected = [];
+            } else if (res.Result.Message != "" && res.Result.ErrCode != "") {
+              this.errorGeneral = { isError: true, errorMessage: res.Result.Message };
+            } else {
+              this.errorGeneral = { isError: true, errorMessage: this.mensajeErrorGenerico };
+            }
+          } else {
+            this.errorGeneral = { isError: true, errorMessage: this.mensajeErrorGenerico };
+          }
+        },
+          err => {
+            this.spinner.hide();
+            console.log(err);
+            this.errorGeneral = { isError: false, errorMessage: this.mensajeErrorGenerico };
+          }
+        ); */
+    }
   }
 
 }
