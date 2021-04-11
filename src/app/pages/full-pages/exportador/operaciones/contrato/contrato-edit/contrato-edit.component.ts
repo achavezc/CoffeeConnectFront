4e6f72e01f4bed91f0277bb5babc64c87c8025d1
Javier Jordan Dávila Terrones <jjordandt@gmail.com>
2021-04-11 18:42:50 -0500

    contrato edición

--- src/app/pages/full-pages/exportador/operaciones/contrato/contrato-edit/contrato-edit.component.ts
+++ src/app/pages/full-pages/exportador/operaciones/contrato/contrato-edit/contrato-edit.component.ts
@@ -11,6 +11,30 @@ export class ContratoEditComponent implements OnInit {
   constructor(private fb: FormBuilder) { }
 
   contratoEditForm: FormGroup;
+  listCondicionEmbarque = [];
+  listPaises = [];
+  listCiudades = [];
+  listProductos = [];
+  listMonedas = [];
+  listTipoProduccion = [];
+  listUniMedida = [];
+  listCertificadora = [];
+  listSacosBulk = [];
+  listCalidad = [];
+  listCertificacion = [];
+  listGrado = [];
+  selectedCondEmbarque: any;
+  selectedPais: any;
+  selectedCiudad: any;
+  selectedProducto: any;
+  selectedMoneda: any;
+  selectedTipoProduccion: any;
+  selectedUniMedida: any;
+  selectedCertificadora: any;
+  selectedSacoBulk: any;
+  selectedCalidad: any;
+  selectedCertificacion: any;
+  selectedGrado: any;
 
   ngOnInit(): void {
     this.LoadForm();
@@ -18,7 +42,35 @@ export class ContratoEditComponent implements OnInit {
 
   LoadForm(): void {
     this.contratoEditForm = this.fb.group({
-
+      razonSocial: [],
+      direccionCabe: [],
+      fecha: [],
+      nroRucCabe: [],
+      nroContrato: [],
+      fechaContrato: [],
+      codCliente: [],
+      cliente: [],
+      floId: [],
+      condicionEmbarque: [],
+      fechaEmbarque: [],
+      fechaFactExp: [],
+      pais: [],
+      ciudad: [],
+      producto: [],
+      moneda: [],
+      precio: [],
+      tipoProduccion: [],
+      unidadMedida: [],
+      certificadora: [],
+      sacosBulk: [],
+      calidad: [],
+      certificacion: [],
+      cantidad: [],
+      grado: [],
+      pesoSacoKG: [],
+      cantidadDefectos: [],
+      cargaContrato: [],
+      responsableComercial: []
     });
   }
 
