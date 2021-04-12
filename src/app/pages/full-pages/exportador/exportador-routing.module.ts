3383ec666da706f9b9a6f0656e6e84b56b0642be
Javier Jordan Dávila Terrones <jjordandt@gmail.com>
2021-04-11 22:52:23 -0500

    avance de la bandeja de procesos

--- src/app/pages/full-pages/exportador/exportador-routing.module.ts
+++ src/app/pages/full-pages/exportador/exportador-routing.module.ts
@@ -5,6 +5,8 @@ import { ClienteComponent } from './operaciones/cliente/cliente.component';
 import { ClienteEditComponent } from './operaciones/cliente/cliente-edit/cliente-edit.component';
 import { ContratoComponent } from './operaciones/contrato/contrato.component';
 import { ContratoEditComponent } from './operaciones/contrato/contrato-edit/contrato-edit.component';
+import { OrdenProcesoEditComponent } from './operaciones/ordenproceso/ordenproceso-edit/orden-proceso-edit.component';
+import { OrdenProcesoComponent } from './operaciones/ordenproceso/orden-proceso.component';
 
 const routes: Routes = [
   {
@@ -48,6 +50,27 @@ const routes: Routes = [
       {
         path: 'contrato/update/:id',
         component: ContratoEditComponent,
+        data: {
+          title: 'Contratos'
+        }
+      },
+      {
+        path: 'ordenproceso/list',
+        component: OrdenProcesoComponent,
+        data: {
+          title: 'Contratos'
+        }
+      },
+      {
+        path: 'ordenproceso/create',
+        component: OrdenProcesoEditComponent,
+        data: {
+          title: 'Contratos'
+        }
+      },
+      {
+        path: 'ordenproceso/update/:id',
+        component: OrdenProcesoEditComponent,
         data: {
           title: 'Contratos'
         }
