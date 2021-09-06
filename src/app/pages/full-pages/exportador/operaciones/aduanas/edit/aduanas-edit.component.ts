bef705e0bdb2eaf8eb3968ed27a9847200832ec0
PamelaHM <chuancahuari@outlook.com>
2021-09-06 09:51:38 -0500

    cafe

--- src/app/pages/full-pages/exportador/operaciones/aduanas/edit/aduanas-edit.component.ts
+++ src/app/pages/full-pages/exportador/operaciones/aduanas/edit/aduanas-edit.component.ts
@@ -472,7 +472,7 @@ export class AduanasEditComponent implements OnInit {
         err => {
           this.spinner.hide();
           console.log(err);
-          this.errorGeneral = { isError: true, errorMessage: this.mensajeErrorGenerico };
+          this.errorGeneral = { isError: false, errorMessage: this.mensajeErrorGenerico };
         }
       );
   }
