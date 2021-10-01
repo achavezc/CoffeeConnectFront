3341e5bec8339b4a46800e47128de076a4560451
jorgeazcarate123@gmail.com <jorgeazcarate123@gmail.com>
2021-10-01 11:21:11 -0500

    Fix Finca - Diagnostico

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,6 +2,6 @@ export const environment = {
   production: false,
   base_url: 'http://localhost:5000/api/'
   //base_url: 'http://mruizb-002-site1.htempurl.com/api/'
-  //base_url: 'http://mruizb-002-site3.htempurl.com/api/'
+ //base_url: 'http://mruizb-002-site3.htempurl.com/api/'
 
 };
