d7093a5d911fa80b2f1f8c0f5f3d0c0b399c57ef
Anderson <achavezc146@gmail.com>
2022-11-06 11:37:36 -0500

    ajustes

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,11 +5,11 @@
 export const environment = {
   production: false,
   
-  base_url: 'http://localhost:5000/api/'
+ // base_url: 'http://localhost:5000/api/'
   
   
  
- //base_url: 'http://mruizb-003-site2.htempurl.com/api/'
+ base_url: 'http://mruizb-003-site2.htempurl.com/api/'
   
 
 };
